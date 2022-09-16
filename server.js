a09406adc3010d80e8656741afd8dc73cfde9476
hendrix <leandrotully@gmail.com>
2022-09-16 17:01:08 -0300

    Manipulando uma blacklist

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 
 console.clear();
 require('dotenv').config();
+require('./redis/blackList.js');
 
 const PORT = process.env.PORT;
 
 const db = require('./database');
 const app = require('./app');
 const routes = require('./rotas');
+
 
 routes(app);
 
